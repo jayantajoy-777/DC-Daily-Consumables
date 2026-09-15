60569b7be4d41250091ff9d209c533ad340e8ea7
jayantajoy-777 <jayantabiswasjoy@gmail.com>
2026-09-16 00:45:05 +0530

    Load mobile product page styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ import './dc-product-reference.css';
 import './dc-collection-interactive.css';
 import './dc-ingredient-icons.css';
 import './dc-product-reference-fit.css';
+import './dc-product-mobile.css';
 import HeroShowcase from './components/HeroShowcase';
 import DCProductDetailsReference from './components/DCProductDetailsReference';
 import DCCollectionInteractive from './components/DCCollectionInteractive';
